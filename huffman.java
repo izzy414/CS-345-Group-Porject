1c2682155f9fa279f033eb127b8129060d359435
izzy414 <149432568+izzy414@users.noreply.github.com>
2023-10-30 16:19:50 -0700

    Create huffman.java

---
+++ huffman.java
@@ -0,0 +1,8 @@
+// Main function that takes the user input
+// Creating of the tree that takes the user input as an argument
+   // Count the freqencies in the string and store them in a new hash map
+// Creates the nodes of the tree
+// Store the huffman codes in a map
+// Encode the data
+// Print the data in binary tree form
+// Print the data in tree form
