0a7b2746b79516dee5a20d7c767875dd52a46534
izzy414 <149432568+izzy414@users.noreply.github.com>
2023-10-30 17:12:36 -0700

    Update huffman.java

--- huffman.java
+++ huffman.java
@@ -1,8 +1,8 @@
 // Main function that takes the user input
-// Creating of the tree that takes the user input as an argument
-   // Count the freqencies in the string and store them in a new hash map
-// Creates the nodes of the tree
-// Store the huffman codes in a map
-// Encode the data
+// Frequencies function gets frequencies of string and puts it into hashmap (takes arguement of the string)
+// Tree function creates the tree (takes the hashmap as an argument)
+   // Node object reates the nodes of the tree with number frequency, letter, L = 2K+1 and R = 2k+2
+   // Max heap array
+   // 
 // Print the data in binary tree form
 // Print the data in tree form
