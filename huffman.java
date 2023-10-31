2cb941fa670530e8653a8deae14745fc92ae458a
izzy414 <149432568+izzy414@users.noreply.github.com>
2023-10-30 17:29:38 -0700

    Update huffman.java

--- huffman.java
+++ huffman.java
@@ -5,5 +5,5 @@
    // Node object reates the nodes of the tree with number frequency, letter, L = 2K+1 and R = 2k+2
    // Puts the letters into nodes and creates third node with sum of it's children all the way to the root
    // Do recursive backtracking to get the bit values and store that value into our hashmap
-// Print the hasmap in binary tree form
+// Print the hashmap in binary tree form
 // Print the hashmap in table form
