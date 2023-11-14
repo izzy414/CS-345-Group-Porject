3bcea2e27e0211e6753c0718bb92c6cad4d9d3f3
izzy414 <149432568+izzy414@users.noreply.github.com>
2023-11-14 15:38:11 -0700

    Delete huffman.java

--- huffman.java
+++
@@ -1,9 +0,0 @@
-// Main function that takes the user input
-// Frequencies function gets frequencies of string and puts it into hashmap (takes arguement of the string)
-// Sort function sorts the hashmap by value in ascending order
-// initTree function creates the tree (takes the sorted hashmap as an argument)
-   // Node object reates the nodes of the tree with number frequency, letter, L = 2K+1 and R = 2k+2
-   // Puts the letters into nodes and creates third node with sum of it's children all the way to the root
-   // Do recursive backtracking to get the bit values and store that value into our hashmap
-// Print the hashmap in binary tree form
-// Print the hashmap in table form
